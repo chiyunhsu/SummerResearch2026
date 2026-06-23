dc3084db307c254d17da1a0dff6223fd1107b287
Chi-Yun Hsu <chiyunhsu@users.noreply.github.com>
2026-06-23 16:14:30 -0700

    First commit

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import SummerResearch2026
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ SummerResearch2026.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `SummerResearch2026` library.
+-- Import modules here that should be built as part of the library.
+import SummerResearch2026.Basic
